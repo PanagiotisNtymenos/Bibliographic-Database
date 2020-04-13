1ffb5730edd1783bcd21379dbff037cd980ae159
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-13 20:40:55 +0300

    Query 2 Done!

--- Part A'/SQL Scripts/Query2.sql
+++ Part A'/SQL Scripts/Query2.sql
@@ -1,13 +1,24 @@
 /* Εμφανίστε τον τίτλο και την γλώσσα των τεκμηρίων πού έχει εκδώσει ο 'Κλειδάριθμος'. */
 
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
 SELECT title, lang FROM Bibrecs 
 JOIN Publishers 
 ON Publishers.pubid = Bibrecs.pubid
 WHERE Publishers.pubname = 'Κλειδάριθμος'
 ORDER BY title
 
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+
 
 /* Εμφανίστε έναν κατάλογο με το όνομα κάθε τμήματος και τον συνολικό αριθμό των δανεισμών που έγιναν από τα μέλη του το έτος 2000. */
+
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
 
 SELECT depname, COUNT(Loanstats.lid) AS loans FROM Departments 
 LEFT JOIN Borrowers 
@@ -17,8 +28,15 @@ ON Borrowers.bid = Loanstats.bid
 WHERE Loanstats.loandate LIKE '2000%'
 GROUP BY depname
 
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+
 
 /* Εμφανίστε έναν κατάλογο με τον τίτλο, την γλώσσα και το όνομα του συγγραφέα (ή των συγγραφέων) των τεκμηρίων στα οποία έχει αποδοθεί ο θεματικός όρος "Databases". */
+
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
 
 SELECT title, lang, author AS loans FROM Bibrecs 
 JOIN Bibauthors 
@@ -32,3 +50,32 @@ ON Bibterms.tid = Sterms.tid
 WHERE term = 'Databases'
 ORDER BY title
 
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+
+
+/* 
+Να δημιουργήσετε κατάλληλα ευρετήρια που θα επιταχύνουν την εκτέλεση των παραπάνω ερωτημάτων. 
+Να παραθέσετε τις εντολές δημιουργίας των ευρετηρίων, καθώς επίσης και στοιχεία που να αποδεικνύουν ότι, 
+τα ευρετήρια που δημιουργήσατε επιταχύνουν την εκτέλεση των ερωτημάτων. 
+*/
+
+/* A */
+
+CREATE INDEX IndxA ON Publishers(pubname)
+
+DROP INDEX Publishers.IndxA;
+
+
+/* B */
+
+CREATE INDEX IndxB ON Loanstats(lid, loandate)
+
+DROP INDEX Loanstats.IndxB;
+
+
+/* C */
+
+CREATE INDEX IndxC ON Sterms(term)
+
+DROP INDEX Publishers.IndxC;

887104647165307446b68b1d5d0877a7399d16c0
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-13 19:14:42 +0300

    Query 1 Made!

--- Part A'/SQL Scripts/Query 1/CreateIndex.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX Indx ON Bibrecs(title);
-
-SELECT title FROM Bibrecs WHERE title LIKE 'Οικ%'
-ORDER BY title
---
+++ Part A'/SQL Scripts/Query1.sql
@@ -0,0 +1,52 @@
+DROP INDEX Bibrecs.Indx;
+
+/* Να δημιουργήσετε κατάλληλο ευρετήριο που να επιταχύνει την εκτέλεση του παρακάτω ερωτήματος. */
+
+CREATE INDEX Indx ON Bibrecs(title);
+
+
+SELECT title FROM Bibrecs WHERE title LIKE 'Οικ%'
+ORDER BY title
+
+
+
+
+/* Εμφάνισε τον τίτλο των τεκμηρίων που περιέχουν στον τίτλο την λέξη πληροφορική. */
+
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
+SELECT title FROM Bibrecs WHERE title LIKE '%Πληροφορική%'
+ORDER BY title
+
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+
+
+/* Εμφάνισε τον τίτλο, και το είδος των τεκμηρίων με τίτλο "Economics". */
+
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
+SELECT title, material FROM Bibrecs WHERE title = 'Economics'
+ORDER BY title
+
+SET STATISTICS TIME OFF
+SET STATISTICS IO OFF
+
+
+/* Εμφάνισε τον τίτλο και το είδος των τεκμηρίων των οποίων ο τίτλος ξεκινάει με την λέξη "Economics". */
+
+DBCC DROPCLEANBUFFERS
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+
+SELECT title, material FROM Bibrecs WHERE title LIKE 'Economics%'
+ORDER BY title
+
+SET STATISTICS TIME OFF
+SET STATISTICS IO OFF
+
+
